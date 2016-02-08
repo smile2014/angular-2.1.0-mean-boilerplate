c9af71b4630919342dbba87bd0e087a868141407
Simon <simonxca@yahoo.ca>
2016-02-08 11:11:11 -0500

    adds a layer of abstraction

--- server.js
+++ server.js
@@ -4,21 +4,32 @@ const express = require('express');
 const glob = require('glob');
 const mongoose = require('mongoose');
 
-// MongoDB
-mongoose.connect(config.db);
-const db = mongoose.connection;
-db.on('error', () => {
-  throw new Error('unable to connect to database at ' + config.db);
-});
-
-const models = glob.sync(config.root + '/app/models/*.js');
-models.forEach((model) => require(model));
-
-// Express
 const app = express();
 
-require('./config/express')(app, config);
+connectToMongo();
+loadModels();
+loadRoutes();
+startServer();
 
-app.listen(config.port, () => {
-  console.log('Express server listening on port ' + config.port);
-});
+function connectToMongo() {
+  mongoose.connect(config.db);
+  const db = mongoose.connection;
+  db.on('error', () => {
+    throw new Error('unable to connect to database at ' + config.db);
+  });
+}
+
+function loadModels() {
+  const models = glob.sync(config.root + '/app/models/*.js');
+  models.forEach((model) => require(model));
+}
+
+function loadRoutes() {
+  require('./config/express')(app, config);
+}
+
+function startServer() {
+  app.listen(config.port, () => {
+    console.log('Express server listening on port ' + config.port);
+  });
+}
