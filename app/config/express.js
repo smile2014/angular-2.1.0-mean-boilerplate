90dfa450effcf2eea6870b65d66feb8aeebffe9e
Simon <simonxca@yahoo.ca>
2016-02-11 16:42:10 -0500

    moved images to an assets folder

--- app/config/express.js
+++ app/config/express.js
@@ -17,7 +17,7 @@ module.exports = function (app, config) {
 };
 
 function loadMiddleware(app, config) {
-  app.use(favicon(config.root + '/public/img/favicon.ico'));
+  app.use(favicon(config.root + '/public/assets/img/favicon.ico'));
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
