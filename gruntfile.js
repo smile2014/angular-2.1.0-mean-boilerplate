a6abaad9f1d861556ee5e0b091121c0a181d0b2a
Simon <simonxca@yahoo.ca>
2016-02-15 16:17:57 -0500

    adds grunt typescript compiler and tsconfig.json for ts projects

--- gruntfile.js
+++ gruntfile.js
@@ -19,16 +19,39 @@ module.exports = function (grunt) {
       files: {
         src: ['app/test/*.js']
       }
+    },
+    
+    ts: {
+      default: {
+        src: ['public/ts/**/*.ts'],
+        outDir: 'public/js',
+        options: {
+          rootDir: 'public/ts',
+          fast: 'never',
+          // from tsconfig.json
+          "target": "es5",
+          "module": "system",
+          "moduleResolution": "node",
+          "sourceMap": true,
+          "emitDecoratorMetadata": true,
+          "experimentalDecorators": true,
+          "removeComments": false,
+          "noImplicitAny": false
+        }
+      }
     }
   });
   
   [
     'grunt-contrib-jshint',
     'grunt-mocha-test',
-    'grunt-mocha-istanbul'
+    'grunt-mocha-istanbul',
+    'grunt-ts'
   ].forEach((task) => grunt.loadNpmTasks(task));
   
   grunt.registerTask('default', ['jshint', 'mocha_istanbul']);
-  grunt.registerTask('test', ['mochaTest']);
+  
   grunt.registerTask('cover', ['mocha_istanbul']);
+  grunt.registerTask('test', ['mochaTest']);
+  grunt.registerTask('tsc', ['ts']);
 };
