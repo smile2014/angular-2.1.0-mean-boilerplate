b7b5e4882243d1d9684a6e6cb4971f86fe689144
Simon <simonxca@yahoo.ca>
2016-03-13 15:26:19 -0400

    removes print object

--- app/ts/utils/express-utils.ts
+++ app/ts/utils/express-utils.ts
@@ -1,13 +1,3 @@
 export function getNodeEnv(): string {
   return process.env.NODE_ENV || 'development';
 }
-
-export function printObject(obj: any): void {
-  for (let property of Object.keys(obj)) {
-    let value: string = obj[property];
-    if (typeof value === 'object') {
-      value = JSON.stringify(value, null, ' ');
-    }
-    console.log(`${property}: ${value}`);
-  }
-}
