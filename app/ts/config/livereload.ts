8cb9cf2cd0948134a27d1da2aea5c299397ad5c8
Simon <simonxca@yahoo.ca>
2016-04-02 11:51:21 -0400

    fixes weird npm watch issue not broadcasting on nested directories

--- app/ts/config/livereload.ts
+++ app/ts/config/livereload.ts
@@ -7,9 +7,11 @@ const livereload = sendevent('/livereload');
 
 module.exports = function (app: express.Express) {
   app.use(livereload);
-  watch.watchTree(`${config.root}/public/`, {
-    ignoreDirectoryPattern: /ts/
-  }, () => {
-    livereload.broadcast({action: 'reload'});
+  // directories to watch for changes
+  [
+    `${config.root}/public/`,
+    `${config.root}/public/js`
+  ].forEach((dir: string) => {
+    watch.watchTree(dir, () => livereload.broadcast({action: 'reload'}));
   });
 };
