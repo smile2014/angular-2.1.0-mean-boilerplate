d987ed8111877aecc19b41a6293ed26e651829a7
Simon <simonxca@yahoo.ca>
2016-07-31 15:41:38 -0400

    adds more router examples

--- public/ts/routes/article/article-route.ts
+++ public/ts/routes/article/article-route.ts
@@ -1,17 +1,26 @@
 import {Component} from '@angular/core';
-import {ROUTER_DIRECTIVES} from '@angular/router';
+import {ROUTER_DIRECTIVES, Router} from '@angular/router';
 
 @Component({
   directives: [ROUTER_DIRECTIVES],
   template: `
   <div>
     <h1>Article Section:</h1>
-    <a [routerLink]="[1]">Article 1</a>
-    <a [routerLink]="[2]">Article 2</a>
+    <a (click)="openArticle(1)">Article 1</a>
+    <a routerLink="2">Article 2</a>
     <a [routerLink]="[3]">Article 3</a>
 
     <router-outlet></router-outlet>
   <div>
   `
 })
-export class ArticleRoute {}
+export class ArticleRoute {
+  constructor(private router: Router) {}
+
+  openArticle(id: number) {
+    this.router.navigate(['article', id, {
+      param1: 'param1',
+      param2: 'param2'
+    }]);
+  }
+}
