377287ade88a93b1331584673f0953fa77cc9efc
Simon <simonxca@yahoo.ca>
2016-10-14 00:14:05 -0400

    add more variety of form elements and attributes

--- public/src/routes/forms/forms.route.ts
+++ public/src/routes/forms/forms.route.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   moduleId: module.id,
+  styleUrls: ['./forms.route.css'],
   templateUrl: './forms.route.html'
 })
 export class FormsRoute {}
